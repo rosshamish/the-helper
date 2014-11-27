c761539672a2082a2bf29f302e5ed29e4dedf9db
ahoskins <arhoskin@ualberta.ca>
2014-11-27 08:41:10 -0700

    first commit from ubuntu

---
+++ ZZ.h
@@ -0,0 +1,21 @@
+#ifndef ZZ_H
+#define ZZ_H
+
+// Standard Lib/Screen
+#include <Arduino.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7735.h>
+#include <SPI.h>
+#include <SD.h>
+#include <lcd_image.h>
+
+// U of A library settings for Atmel SPI
+#define SD_CS   5
+#define TFT_CS  6
+#define TFT_DC  7
+#define TFT_RST 8
+
+// Class header
+#include "ZZSprite.h"
+
+#endif
---
+++ testing.cpp
@@ -0,0 +1,43 @@
+// Include the library
+#include "ZZ.h"
+
+// Display
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
+
+// Edmonton map image
+lcd_image_t cur_image = { "yeg-big.lcd", 2048, 2048 };
+
+Sd2Card card;
+
+void setup() {
+	/*
+	To the basic setup
+	*/
+	Serial.begin(9600);
+	tft.initR(INITR_REDTAB);
+	Serial.print("Init..");
+
+	// It worked...
+	Serial.println("Done, init, all is ok");
+
+	// If failed...
+	if (!SD.begin(SD_CS)) {
+		Serial.print("failed to init SD");
+		return;
+	}
+	
+	if (!card.init(SPI_HALF_SPEED, SD_CS)) {
+    	Serial.println("Raw SD Initialization has failed");
+    	while (1) {};
+  	}
+
+  	/*
+  	Test my Class's functionality
+  	*/
+	ZZSprite mySprite;
+	mySprite.addImage(cur_image);
+	mySprite.drawSprite(tft);
+}
+
+void loop() {
+}
