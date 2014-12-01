2044badab44e368ec095de8ca81d44df01565903
ahoskins <arhoskin@ualberta.ca>
2014-12-01 16:55:40 -0700

    tests reflect functionality

--- testing.cpp
+++ testing.cpp
@@ -1,15 +1,9 @@
 // Include the library
 #include "ZZ.h"
 
-// Display
-Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
-
 void setup() {
-	// Make a scene first
-	// Create some shape and add to this scene
-	// Create a joystick with a screen instance passed in
-	// Store references to scenes in the joystick
-	// When the joystick moves the delegate, check these scenes
+	// Display
+	Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 
 	/*
 	The the basic setup
@@ -17,91 +11,140 @@ void setup() {
 	Serial.begin(9600);
 	tft.initR(INITR_REDTAB);
 	Serial.print("Init..");
-
-  	tft.fillScreen(0);
+	tft.fillScreen(0);
 
 	/*
 	Create some shapes
 	*/
-	Rectangle one (10, 30, 5, 20, &tft, 0xFF00);
-	Rectangle two (30, 30, 5, 20, &tft, 0xFFFF);
-	Rectangle three (50, 30, 5, 20, &tft, 0xFFFF);
-	Rectangle four (100, 100, 5, 20, &tft, 0xFFFF);
-	Rectangle five (70, 100, 5, 20, &tft, 0xFFFF);
+	Rectangle delegateOne (10, 30, 5, 20, &tft, 0xFF00);
+	Rectangle portalShape (30, 30, 5, 20, &tft, 0xFFFF);
+	Rectangle changeColorShape (50, 30, 5, 20, &tft, 0xFFFF);
+	Rectangle spawnNewShape (50, 90, 10, 10, &tft, 0xFFFF);
+	Rectangle spawnedShape (70, 90, 10, 10, &tft, 0xFFFF);
+
+	Rectangle delegateTwo (100, 100, 5, 20, &tft, 0xFF00);
+	Rectangle portalShapeTwo (70, 100, 5, 20, &tft, 0xFFFF);
 
 	// you can draw a shape individually
-	one.drawShape();
+	delegateOne.drawShape();
 
 	// and hide a shape individually
-	one.hideShape();
-
+	delegateOne.hideShape();
 
 	/*
 	Create some scenes and add some shapes to them
 	So shapes can live alone, or in a scene
 	*/
-	Scene myScene;
-	myScene.addToScene(&one);
-	myScene.addToScene(&two);
-	myScene.addToScene(&three);
+	Scene firstScene;
+	firstScene.addToScene(&delegateOne);
+	firstScene.addToScene(&portalShape);
+	firstScene.addToScene(&changeColorShape);
+	firstScene.addToScene(&spawnNewShape);
 
 	Scene secondScene;
-	secondScene.addToScene(&four);
-	secondScene.addToScene(&five);
+	secondScene.addToScene(&delegateTwo);
+	secondScene.addToScene(&portalShapeTwo);
 
 	// draw an entire scene
-	myScene.drawScene(true);
-
+	firstScene.drawScene(true);
 
 	/*
-	Attach joystick to a delegate within a scene
+	Create joystick and attach to a delegate within a scene
 	*/
 	JoyStick myJoy (&tft);
-	myJoy.addHandle(&myScene, &one);
+	myJoy.addHandle(&firstScene, &delegateOne);
 
-	/*
-	Check for collisions within this scene
-	Respond to the collision by changing the active scene
-	*/
 	bool toggleState = true;
 
-	Rectangle* collidedShape = NULL;
+	/*
+	Simulate game play with portalShape different scenes
+	*/
 	while (1) {
-		collidedShape = myJoy.adjustPosition();
-		if (collidedShape != NULL) {
-			one.setColor(0xFFFF);
-			Serial.println("Collision alerted.");
-			collidedShape = NULL;
+		Rectangle* collidedShape = NULL;
+		// Scene delegateOne event logic
+		while (toggleState) {
+			Rectangle* collidedShape = NULL;
+			collidedShape = myJoy.adjustPosition();
 
-			if (toggleState) {
+			// Collision with portalShape
+			if (collidedShape == &portalShape) {
 				// Hide the current scene
-				myScene.hideScene();
+				firstScene.hideScene();
 
 				// Draw the new scene in original state
 				secondScene.drawScene(true);
 
 				// Attach the new delegate and scene
-				myJoy.addHandle(&secondScene, &four);
+				myJoy.addHandle(&secondScene, &delegateTwo);
 				toggleState = false;
 			}
-			else {
-				secondScene.hideScene();
-				myScene.drawScene(true);
-				myJoy.addHandle(&myScene, &one);
 
+			if (collidedShape == &changeColorShape) {
+				// change the color of changeColorShape
+				changeColorShape.setColor(0x0033);
+				changeColorShape.drawShape();
+			}
+
+			if (collidedShape == &spawnNewShape) {
+				// add the new shape to the scene
+				firstScene.addToScene(&spawnedShape);
+				firstScene.drawScene(false);
+			}
+
+
+		}
+
+		// Scene portalShape event logic
+		while (!toggleState) {
+			Rectangle* collidedShape = NULL;
+			collidedShape = myJoy.adjustPosition();
+
+			if (collidedShape == &portalShapeTwo) {
+				// jump back to the other scene
+				secondScene.hideScene();
+
+				firstScene.drawScene(true);
+
+				myJoy.addHandle(&firstScene, &delegateOne);
 				toggleState = true;
 			}
-		}
+		}	
 	}
 
-	// It only makes sense to have JoyStick delegate one object at a time
-	// Check collision between delegate and other objects in its scene
-	// It probably makes sense to automatically add the delegates scene to the joystick
-	// So scenes are packets of shapes. We can add a shape to be delegated, and then
-	// all of the shapes in the delegated shapes scene are check for collision.
+	/*
+	Check for collisions within this scene
+	Respond to the collision by changing the active scene
+	*/
+	// bool toggleState = true;
 
-	// Obviously, a shape should be allowed to be in multiple scenes
-	// Add the scene...then add a shape from the scene.  So it's good how it is now.
+	// Rectangle* collidedShape = NULL;
+	// while (1) {
+	// 	collidedShape = myJoy.adjustPosition();
+	// 	if (collidedShape != NULL) {
+	// 		delegateOne.setColor(0xFFFF);
+	// 		Serial.println("Collision alerted.");
+	// 		collidedShape = NULL;
+
+	// 		if (toggleState) {
+	// 			// Hide the current scene
+	// 			firstScene.hideScene();
+
+	// 			// Draw the new scene in original state
+	// 			secondScene.drawScene(true);
+
+	// 			// Attach the new delegate and scene
+	// 			myJoy.addHandle(&secondScene, &delegateTwo);
+	// 			toggleState = false;
+	// 		}
+	// 		else {
+	// 			secondScene.hideScene();
+	// 			firstScene.drawScene(true);
+	// 			myJoy.addHandle(&firstScene, &delegateOne);
+
+	// 			toggleState = true;
+	// 		}
+	// 	}
+	// }
 
 }
 
